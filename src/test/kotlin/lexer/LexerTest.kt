e696c4d8cd05c57b6c5e686fae1082a1927804b3
lag129 <65485662+lag129@users.noreply.github.com>
2024-09-24 17:40:30 +0900

    テストケースを追加

--- src/test/kotlin/lexer/LexerTest.kt
+++ src/test/kotlin/lexer/LexerTest.kt
@@ -7,25 +7,90 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class LexerTest {
-
-    private val input = "=+(){},;"
-
-    private val tests = listOf(
-        Token(TokenType.ASSIGN, "="),
-        Token(TokenType.PLUS, "+"),
-        Token(TokenType.LPAREN, "("),
-        Token(TokenType.RPAREN, ")"),
-        Token(TokenType.LBRACE, "{"),
-        Token(TokenType.RBRACE, "}"),
-        Token(TokenType.COMMA, ","),
-        Token(TokenType.SEMICOLON, ";"),
-        Token(TokenType.EOF, ""),
-    )
-
     @Test
     fun testNextToken() {
-        val lexer = Lexer(input)
+        val input = "=+(){},;"
+        val tests = listOf(
+            Token(TokenType.ASSIGN, "="),
+            Token(TokenType.PLUS, "+"),
+            Token(TokenType.LPAREN, "("),
+            Token(TokenType.RPAREN, ")"),
+            Token(TokenType.LBRACE, "{"),
+            Token(TokenType.RBRACE, "}"),
+            Token(TokenType.COMMA, ","),
+            Token(TokenType.SEMICOLON, ";"),
+            Token(TokenType.EOF, ""),
+        )
 
+        val lexer = Lexer(input)
+        tests.forEach { expectedToken ->
+            val token = lexer.nextToken()
+            assertEquals(
+                expectedToken.type,
+                token.type,
+                "期待されたTokenのTypeが異なります"
+            )
+            assertEquals(
+                expectedToken.literal,
+                token.literal,
+                "期待されたTokenのLiteralが異なります"
+            )
+        }
+    }
+
+    @Test
+    fun testNextToken2() {
+        val input = """
+        let five = 5;
+        let ten = 10;
+        
+        let add = fn(x, y) {
+            x + y;
+        };
+        
+        let result = add(five, ten);
+        """
+        val tests = listOf(
+            Token(TokenType.LET, "let"),
+            Token(TokenType.IDENT, "five"),
+            Token(TokenType.ASSIGN, "="),
+            Token(TokenType.INT, "5"),
+            Token(TokenType.SEMICOLON, ";"),
+            Token(TokenType.LET, "let"),
+            Token(TokenType.IDENT, "ten"),
+            Token(TokenType.ASSIGN, "="),
+            Token(TokenType.INT, "10"),
+            Token(TokenType.SEMICOLON, ";"),
+            Token(TokenType.LET, "let"),
+            Token(TokenType.IDENT, "add"),
+            Token(TokenType.ASSIGN, "="),
+            Token(TokenType.FUNCTION, "fn"),
+            Token(TokenType.LPAREN, "("),
+            Token(TokenType.IDENT, "x"),
+            Token(TokenType.COMMA, ","),
+            Token(TokenType.IDENT, "y"),
+            Token(TokenType.RPAREN, ")"),
+            Token(TokenType.LBRACE, "{"),
+            Token(TokenType.IDENT, "x"),
+            Token(TokenType.PLUS, "+"),
+            Token(TokenType.IDENT, "y"),
+            Token(TokenType.SEMICOLON, ";"),
+            Token(TokenType.RBRACE, "}"),
+            Token(TokenType.SEMICOLON, ";"),
+            Token(TokenType.LET, "let"),
+            Token(TokenType.IDENT, "result"),
+            Token(TokenType.ASSIGN, "="),
+            Token(TokenType.IDENT, "add"),
+            Token(TokenType.LPAREN, "("),
+            Token(TokenType.IDENT, "five"),
+            Token(TokenType.COMMA, ","),
+            Token(TokenType.IDENT, "ten"),
+            Token(TokenType.RPAREN, ")"),
+            Token(TokenType.SEMICOLON, ";"),
+            Token(TokenType.EOF, ""),
+        )
+
+        val lexer = Lexer(input)
         tests.forEach { expectedToken ->
             val token = lexer.nextToken()
             assertEquals(
